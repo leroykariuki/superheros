7dc23c669d2ee03819c02d46ccf8f29836bdb915
leroy <leroy.kariuki@student.moringaschool.com>
2024-01-17 12:40:34 +0300

    initial

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from models import db, Hero, Power, HeroPower
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -12,6 +13,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 migrate = Migrate(app, db)
 db.init_app(app)
 api = Api(app)
+CORS(app)
 
 
 @app.route('/')
